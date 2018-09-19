b02a6e4b44e8bea078a5910a97c533ac13bc912b
Amit Kumar <akchoudhary966@gmail.com>
2018-09-19 08:29:37 +0530

    Project Structure chnaged

---
+++ api/mongo/driver.go
@@ -0,0 +1,16 @@
+package mongo
+
+var MaxPool int
+var PATH string
+var DBNAME string
+
+func CheckAndInitServiceConnection() {
+	if service.baseSession == nil {
+		service.URL = PATH
+		err := service.New()
+		if err != nil {
+			panic(err)
+		}
+
+	}
+}
